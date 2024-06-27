dc177a9dc3008621ac37b4cbed3de9b070706dc1
SBMOYO <sunganomoyo1@gmail.com>
2024-06-27 16:18:46 +0200

    create chat functionaly with channels

--- HarvestHub/settings.py
+++ HarvestHub/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'notify',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +73,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'HarvestHub.wsgi.application'
+
+ASGI_APPLICATION = 'HarvestHub.asgi.application'
 
 
 # Database
@@ -121,3 +128,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Channels
+ASGI_APPLICATION = "HarvestHub.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
--- notify/views.py
+++ notify/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "notify/index.html")
+
+def room(request, room_name):
+    return render(request, "notify/room.html", {"room_name": room_name})
