ee683e2082207f3b4c30ede9b78f7e1f48b6c768
SBMOYO <sunganomoyo1@gmail.com>
2024-07-16 12:38:34 +0200

    update feature Market-Insights

--- notify/models.py
+++ notify/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class WhatsAppUser(models.Model):
+    phone_number = models.CharField(max_length=15, unique=True)
+    profile_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.phone_number
--- notify/urls.py
+++ notify/urls.py
@@ -1,9 +1,7 @@
 from . import views
 from django.urls import path
-from . import routing
+from . import celery
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path("<str:room_name>/", views.room, name="room"),
     path("twilio_webhook/", views.handle_incoming_whatsapp, name="twilio_webhook"),
 ]
